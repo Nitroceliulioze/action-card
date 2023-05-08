7b1d6c61728aedcd59bc976104af70a95f262177
Nitroceliulioze <aksionova.diana@gmail.com>
2023-05-08 21:15:20 +0300

    Add text inputs (colors doesn't work)

--- src/app/components/action-card-component/action-card-component.component.ts
+++ src/app/components/action-card-component/action-card-component.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-action-card-component',
   templateUrl: './action-card-component.component.html',
-  styleUrls: ['./action-card-component.component.scss']
+  styleUrls: ['./action-card-component.component.scss'],
 })
 export class ActionCardComponentComponent {
   isListOpen: boolean = false;
+  @Input() titleColor!: string;
+  //  @Input() rowData Array of objects needs interface
+  @Input() badge!: string;
+  @Input() addActivityText!: string;
+  @Input() deleteText!: string;
 
   toggleList() {
-    this.isListOpen = !this.isListOpen
+    this.isListOpen = !this.isListOpen;
   }
 }
