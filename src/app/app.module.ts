be0d5b7ec27fc1ef41274f6fb247485b6a04a5e2
Nitroceliulioze <aksionova.diana@gmail.com>
2023-05-08 14:52:00 +0300

    add card rows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ActionCardComponentComponent } from './components/action-card-component/action-card-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ActionCardComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/action-card-component/action-card-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-action-card-component',
+  templateUrl: './action-card-component.component.html',
+  styleUrls: ['./action-card-component.component.scss']
+})
+export class ActionCardComponentComponent {
+
+}
