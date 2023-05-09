7be6c093b3396978a958eaee47b83a443c8a7a75
Nitroceliulioze <aksionova.diana@gmail.com>
2023-05-09 12:06:33 +0300

    create ActionCardContentInterface

---
+++ src/app/components/action-card-component/action-card-content-interface.ts
@@ -0,0 +1,8 @@
+export interface ActionCardContentInterface {
+    title: string;
+    addActivity: string; // ??????
+    rowTitle: string;
+    badge: string;
+    isDeletableAction?: boolean;
+    rowList: string[];
+}
