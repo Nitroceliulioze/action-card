e4aa2287b0fcfd4bc67799de33bf72713605e994
Nitroceliulioze <aksionova.diana@gmail.com>
2023-05-11 14:35:01 +0300

    make hr hidden after last child in a row

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { ActionCardContentInterface } from '../action-card-component/action-card-content-interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
- 
-}
+export class HomeComponent {}
