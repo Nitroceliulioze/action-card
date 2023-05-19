a5f89ff3d7396d0d9951428dcd88433a937dce32
Nitroceliulioze <aksionova.diana@gmail.com>
2023-05-19 15:45:15 +0300

    deleted extra tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,20 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'action-card'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('action-card');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    const actionCardComponent = compiled.querySelector('app-action-card-component');
-    const titleElement = actionCardComponent.querySelector('.card-title');
-
-    expect(titleElement.textContent).toContain(actionCardComponent.title);
-  });
 });
