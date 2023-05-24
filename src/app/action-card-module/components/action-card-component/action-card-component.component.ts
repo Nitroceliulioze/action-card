c12fb756ffb03ad132c768ccfcbe18142adabcca
Nitroceliulioze <aksionova.diana@gmail.com>
2023-05-24 13:54:15 +0300

    add bootstrap script tag in index; style stories

--- src/app/action-card-module/components/action-card-component/action-card-component.component.ts
+++ src/app/action-card-module/components/action-card-component/action-card-component.component.ts
@@ -6,11 +6,9 @@ import { ActionCardContentInterface } from './action-card-content-interface';
   templateUrl: './action-card-component.component.html',
   styleUrls: ['./action-card-component.component.scss'],
 })
-export class ActionCardComponentComponent {
-  @Input() isDeletableAction!: boolean;
+export class ActionCardComponentComponent {  
   @Input() title!: string;
   @Input() titleColor!: string;
   @Input() addActivityText!: string;
-  @Input() deleteText!: string;
   @Input() cardContentRow: ActionCardContentInterface[] = [];
 }
